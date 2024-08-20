8c4f8f34b8275cffe867bd0c3a7f9fef2838f98f
Ibrahim <ibrra187@gmail.com>
2024-08-20 22:43:05 +0200

    Added BanRecord class, which holds all ban information. Added JSONReader class, which reads the JSON and has methods which return a BanRecord for a selected player. Currently only works with SteamIDs. Will need to implement other forms of identification later.
    Code has one redundancy in that steamID needs to be stored twice. Band-aid workaround for now but the code should still work. Untested due to IDE issue.

---
+++ Readers/JSONReader.cs
@@ -0,0 +1,53 @@
+﻿namespace AutoPunisher;
+
+using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;  //For .NET use
+using System.Text.Json;	//For non-.NET use
+//pick one of them
+using Events;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+
+public class JSONReader
+{
+    static JObject wholeJsonFile;
+    static string currentFileName;
+    
+    public JSONReader()
+	{
+        
+    }
+
+    //Reads the JSON file by using a streamreader. This method is to be called on awake in the ReasonReader class. - Messenger
+	public static void ReadJson(string fileName)
+	{
+        currentFileName = fileName;
+        StreamReader file = File.OpenText(fileName);
+        JsonTextReader reader = new JsonTextReader(file);
+        wholeJsonFile = JObject.Load(reader);
+    }
+
+    public static BanRecord GetPlayerBySteamID(string steamID)
+    {
+        //Add code here to create an empty ban record if the steamID is not found. - Messenger
+        JObject obj = (JObject)wholeJsonFile.GetValue(steamID);
+        return GetBanRecord(obj);
+    }
+
+    public static BanRecord GetBanRecord(JObject player)
+    {
+        //Converts the info in the JSON file into primitive C# types. - Messenger
+        string id = Convert.ToString(player.GetValue("playerID"));
+        string name = Convert.ToString(player.GetValue("name"));
+        int racismBans = Convert.ToInt32(player.GetValue("racismBans"));
+
+        //Uses said type to return a ban record.
+        BanRecord playerBanRecord = new BanRecord(id, name, racismBans);
+        reutrn playerBanRecord;
+    }
+
+    public static JObject WholeJsonFile { get { return wholeJsonFile; } }
+    public static string CurrentFileName { get { return currentFileName; } }
+}
